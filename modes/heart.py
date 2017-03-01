37db054a6ab72972c3a3b3dd479f029f45ca2373
MikAnd <micheli.andrea@gmail.com>
2017-03-01 18:17:53 +0100

    Added support for colorized hearts and to variate the number of hearts in heart mode

--- modes/heart.py
+++ modes/heart.py
@@ -3,55 +3,73 @@ import os.path
 import random
 
 class Heart(object):
-    def __init__(self, radius):
-        self.radius = radius
+    def __init__(self, assets_path):
+        self.heart = pygame.image.load(os.path.join(assets_path, "heart.png"))
+        self.heartrect = self.heart.get_rect()
+        self.radius = self.heartrect.width / 2
 
-        self.last_pos = [random.randint(radius, 800-radius), random.randint(radius, 600-radius)]
+        self.color = (random.randrange(256), random.randrange(256),
+                      random.randrange(256), 0)
+        # zero out RGB values
+        self.heart.fill((0, 0, 0, 255), None, pygame.BLEND_RGBA_MULT)
+        # add in new RGB values
+        self.heart.fill(self.color, None, pygame.BLEND_RGBA_ADD)
+
+
+
+        self.last_pos = [random.randint(self.radius, 800-self.radius),
+                         random.randint(self.radius, 600-self.radius)]
         self.speed = [random.randint(-6, 6), random.randint(-6, 6)]
         self.rotation = random.uniform(0, 360)
         self.rotation_speed = random.randint(-2, 2)
 
-    def loop(self, screen, ball):
-        rball = pygame.transform.rotate(ball, self.rotation)
+
+    def loop(self, screen):
+        rheart = pygame.transform.rotate(self.heart, self.rotation)
         self.rotation = (self.rotation + self.rotation_speed) % 360
-        rballrect = rball.get_rect()
-        rballrect.center = self.last_pos
-        rballrect = rballrect.move(self.speed)
-        if rballrect.center[0] - self.radius  <= 0 or rballrect.center[0] + self.radius >= screen.get_width():
+        rheartrect = rheart.get_rect()
+        rheartrect.center = self.last_pos
+        rheartrect = rheartrect.move(self.speed)
+        if rheartrect.center[0] - self.radius  <= 0 or \
+           rheartrect.center[0] + self.radius >= screen.get_width():
             self.speed[0] = -self.speed[0]
             self.speed[1] += random.choice([-1, 0, 1])
             self.rotation_speed += random.choice([-2, -1, 0, 1, 2])
             self.rotation_speed = max(-6, self.rotation_speed)
             self.rotation_speed = min(6, self.rotation_speed)
 
-        elif rballrect.center[1] - self.radius <= 0 or rballrect.center[1] + self.radius >= screen.get_height():
+        elif rheartrect.center[1] - self.radius <= 0 or \
+             rheartrect.center[1] + self.radius >= screen.get_height():
             self.speed[1] = -self.speed[1]
             self.speed[0] += random.choice([-1, 0, 1])
             self.rotation_speed += random.choice([-2, -1, 0, 1, 2])
             self.rotation_speed = max(-6, self.rotation_speed)
             self.rotation_speed = min(6, self.rotation_speed)
 
-        screen.blit(rball, rballrect)
-        self.last_pos = rballrect.center
+        screen.blit(rheart, rheartrect)
+        self.last_pos = rheartrect.center
 
 
 class HeartMode(object):
 
     def __init__(self, assets_path, num_hearts=5):
-        self.ball = pygame.image.load(os.path.join(assets_path, "heart.png"))
-        self.ballrect = self.ball.get_rect()
-        self.radius = self.ballrect.width / 2
-
+        self.assets_path = assets_path
         self.hearts = []
         for _ in range(num_hearts):
-            self.hearts.append(Heart(self.radius))
+            self.hearts.append(Heart(assets_path))
 
     def loop(self, screen):
         for h in self.hearts:
-            h.loop(screen, self.ball)
+            h.loop(screen)
 
     def process_event(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key in [pygame.K_PLUS, pygame.K_KP_PLUS]:
+                self.hearts.append(Heart(self.assets_path))
+            if event.key in [pygame.K_MINUS, pygame.K_KP_MINUS]:
+                if len(self.hearts) > 0:
+                    self.hearts.pop(random.randrange(len(self.hearts)))
+
 
     def preferred_fps(self):
         return 60
